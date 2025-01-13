7f56cc8d7b68c3ec44b3978be5f2be70c691e3ce
Abel Silva <abel2901@gmail.com>
2025-01-12 23:16:28 -0300

    Cria novo arquivo release

--- obj/Debug/net6.0/TodoList.AssemblyInfo.cs
+++ obj/Debug/net6.0/TodoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15353cb79a2d4f297b3153e38e72b68e302ccf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746b4ea9f3bff390bbc1e09d5b0b97ce5c428a11")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
