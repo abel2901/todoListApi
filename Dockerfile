ba31c4d13e0646c34c09d3b432f25cb9630a6e5d
Abel Silva <abel2901@gmail.com>
2025-01-16 00:09:58 -0300

    adiciona dockerfile

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Usar a imagem base do .NET SDK
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+
+# Setar o diretório de trabalho no container
+WORKDIR /app
+
+# Copiar o arquivo de projeto e restaurar as dependências
+COPY *.csproj .
+RUN dotnet restore
+
+# Copiar todo o código e construir a aplicação
+COPY . .
+RUN dotnet publish -c Release -o out
+
+# Gerar a imagem final para a aplicação
+FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS base
+WORKDIR /app
+COPY --from=build /app/out . 
+
+# Expor a porta em que a API vai rodar
+EXPOSE 80
+
+# Comando para rodar a aplicação
+ENTRYPOINT ["dotnet", "TodoList.dll"]
--- obj/Debug/net6.0/TodoList.AssemblyInfo.cs
+++ obj/Debug/net6.0/TodoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5700659786c36448c7326fcf6f398c74c051bbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde55efa46f5c0e91d0b264304615ea0d8a8f1f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
