670a79aa1c816b892f2fd01b9dd8023f67cf86f8
Abel Silva <abel2901@gmail.com>
2025-09-07 19:06:10 -0300

    feat: altera o appsettings

--- obj/Release/net6.0/TodoList.AssemblyInfo.cs
+++ obj/Release/net6.0/TodoList.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46f0051e6f27c8b39d6b2afe32e3b64355c75e32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1af472ec66510d3e2ea646b5a47c9af1aa2a15a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
