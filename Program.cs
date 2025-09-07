766ab5f893b94fbd85962dd76a26077c89b8a5e2
Abel Silva <abel2901@gmail.com>
2025-09-07 18:59:36 -0300

    feat: comenta httpsRedirection

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseHttpsRedirection();
+ //   app.UseHttpsRedirection();
 
 }
 
